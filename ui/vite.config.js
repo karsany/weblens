20d9da2178bebdcae3ab294423206853a89f0ffb
Ferenc Karsany <karsany@karsany.hu>
2023-02-15 23:28:48 +0100

    vite proxy api calls to local cam

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -6,4 +6,9 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), viteSingleFile()],
+  server: {
+    proxy: {
+      '/api': 'http://weblens.local/'
+    }
+  }
 })
